b23496d9f2301da5d15b31bc9db9b1d67a762995
esilfies73 <116110016+esilfies73@users.noreply.github.com>
2026-04-24 13:56:30 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+# Import python packages.
+import streamlit as st
+
+# Write directly to the app.
+st.title(f"Zena's Amazing Athleisure Catalog")
+st.write(
+  """Pick a sweatsuit color or style:
+  """
+)
+
