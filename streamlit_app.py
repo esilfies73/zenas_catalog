1b1532b7effd36829662482003afffe3eb39da62
esilfies73 <116110016+esilfies73@users.noreply.github.com>
2026-04-24 14:19:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,22 @@ import pandas as pd
 
 # Write directly to the app.
 st.title(f"Zena's Amazing Athleisure Catalog")
-st.write(
-  """Pick a sweatsuit color or style:
-  """)
+#st.write(
+#  """Pick a sweatsuit color or style:
+#  """)
 
 #name_on_order = st.text_input("Name on Smoothie:")
 #st.write("The name on your Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('COLOR_OR_STYLE'))
+my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('COLOR_OR_STYLE'),col('FILE_URL))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 sweatsuit_list = st.selectbox(
     'Pick a sweatsuit color or style:'
@@ -38,7 +38,7 @@ if sweatsuit_list:
         search_on=pd_df.loc[pd_df['color_or_style'] == color_chosen].iloc[0]
         st.write('The search value for ', color_chosen,' is ', color_chosen, '.')
         
-        st.subheader(color_chosen + ' Nutrition Information')
+        #st.subheader(color_chosen + ' Nutrition Information')
         image_response = requests.get(f"{file_url}")
         sf_df = st.dataframe(data=image_response.json(),use_container_width=True)
     #st.write(ingredients_string)
